40fa58df79f867e896b54c69e30f1a21bb5baab9
Caio Vinícius Rodrigues Moreira <caiobiscas@gmail.com>
2025-02-11 09:30:51 -0300

    initial setup

---
+++ src/main/java/com/caio/uol/UolApplication.java
@@ -0,0 +1,14 @@
+package com.caio.uol;
+
+import gg.jte.TemplateOutput;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UolApplication.class, args);
+	}
+
+}
