7ad838f8a02e40fe39dd59eb00e6c2b02e20b04d
Caio Vinícius Rodrigues Moreira <caiobiscas@gmail.com>
2025-03-01 23:58:21 -0300

    add unit test editJogador()

--- src/test/java/com/caio/uol/service/JogadorServiceTest.java
+++ src/test/java/com/caio/uol/service/JogadorServiceTest.java
@@ -1,13 +1,15 @@
 package com.caio.uol.service;
 
 import com.caio.uol.domain.Jogador;
+import com.caio.uol.domain.enumeration.Time;
 import com.caio.uol.infra.db.JogadorRepository;
+import com.caio.uol.web.dto.JogadorCreateRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 import java.util.Optional;
@@ -60,5 +62,66 @@ public class JogadorServiceTest {
         assertEquals(expectedJogador, actualJogador);
         verify(jogadorRepository, times(1)).findByName(name);
     }
+
+
+    @Test
+    void editJogador_ExistingJogador_thenEdit(){
+        //given
+        UUID existingJogadorUuid = UUID.randomUUID();
+        Jogador existingJogador = new Jogador();
+        existingJogador.setUuid(existingJogadorUuid);
+        existingJogador.setNome("Caio");
+        existingJogador.setEmail("caio@gmail.com");
+        existingJogador.setTelefone("8888-4444");
+        existingJogador.setTime(Time.VINGADORES);
+
+        JogadorCreateRequest jogadorRequest = new JogadorCreateRequest(existingJogadorUuid.toString(), "Caio V", "caiov@gmail.com", "4444-8888", Time.VINGADORES.getName());
+        when(jogadorRepository.findByUuid(existingJogadorUuid.toString()))
+                .thenReturn(Optional.of(existingJogador));
+        when(jogadorRepository.save(Mockito.any(Jogador.class))).thenAnswer(
+                (i) -> i.getArguments()[0]);
+
+        //when
+        Jogador actualJogador = jogadoresService.editJogador(jogadorRequest);
+
+        // Then
+        assertNotNull(actualJogador);
+
+        ArgumentCaptor<Jogador> jogadorCaptor = ArgumentCaptor.forClass(Jogador.class);
+        verify(jogadorRepository, times(1)).save(jogadorCaptor.capture());
+
+        Jogador savedJogador = jogadorCaptor.getValue();
+        assertEquals(existingJogadorUuid, savedJogador.getUuid());
+        assertEquals(jogadorRequest.nome(), savedJogador.getNome());
+        assertEquals(jogadorRequest.email(), savedJogador.getEmail());
+        assertEquals(jogadorRequest.telefone(), savedJogador.getTelefone());
+        assertEquals(jogadorRequest.time(), savedJogador.getTime().getName());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
